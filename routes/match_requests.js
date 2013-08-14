29b0db5f3975c652057e48bc9209df82cc948427
Sheel Choksi <sheelchoksi@gmail.com>
2013-08-13 20:26:43 -0700

    Accept/reject matches functionality
    still needs lots of cleanup

--- routes/match_requests.js
+++ routes/match_requests.js
@@ -12,43 +12,78 @@ exports.init = function init(app, options) {
   var tableAreas = options.tableAreas,
       probabilisticTableBuckets = makeProbabilisticBuckets(tableAreas);
 
-  app.post('/matches', function(req, res){
+  app.post('/match_requests', function(req, res){
     console.log(req.body);
-
-    function guid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
-        var rand = Math.random()*16|0, charReplacement = (char == 'x') ? rand : (rand&0x3|0x8);
-
-        return charReplacement.toString(16);
-      });
-    }
-
-    var requestGuid = guid();
+    var requestGuid = createGuid();
     setTimeout(function() { scheduleMatch(req.body, requestGuid) }, 0);
     saveMatch(requestGuid, req.body, function() { res.send({guid: requestGuid}) });
   });
 
-  app.get('/matches/:guid', function(req, res){
+  app.get('/match_requests/:guid', function(req, res){
     console.log('polling received for: ' + req.params.guid);
-    redisClient.hgetall(req.params.guid, function(err, savedMatch){
-      if(!savedMatch) {
+    redisClient.hgetall(req.params.guid, function(err, savedMatchRequest){
+      if(!savedMatchRequest) {
         res.status(404).send({});
         return;
       }
 
-      if(savedMatch.opponentNames) {
-        res.send({opponentNames: savedMatch.opponentNames, assignedTable: savedMatch.tableName});
+      if(savedMatchRequest.scheduledMatchGuid) {
+        res.send({scheduledMatchGuid: savedMatchRequest.scheduledMatchGuid});
       } else {
         res.status(304).send({});
       }
     });
   });
 
-  var saveMatch = function(guid, matchDetails, callback){
+  app.get('/matches/:guid', function(req, res){
+    redisClient.hgetall(req.params.guid, function(err, match){
+      if(!match) {
+        res.status(404).send({});
+        return;
+      }
+
+      redisClient.hgetall(match.team1, function(err, team1){
+        redisClient.hgetall(match.team2, function(err, team2){
+          redisClient.ttl(req.params.guid, function(err, ttl){
+            var response = {timeRemaining: ttl, assignedTable: match.tableName};
+            response.teams = [{names: team1.names, confirmed: team1.confirmed}, {names: team2.names, confirmed: team2.confirmed}];
+            response.scheduled = (team1.confirmed < 0 || team2.confirmed < 0) ? -1 : team1.confirmed * team2.confirmed;
+
+            res.send(response);
+          }); 
+        });
+      });
+    });
+  });
+
+  app.put('/matches/:guid', function(req, res){
+    redisClient.hgetall(req.params.guid, function(err, match){
+      if(!match) {
+        res.status(404).send({});
+        return;
+      }
+
+      var confirmedState = req.body.accepted ? 1 : -1;
+
+      redisClient.hset(req.body.matchRequestGuid, 'confirmed', confirmedState, function(err, response) {
+        res.send({});
+      });
+    });
+  });
+
+  function createGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+      var rand = Math.random()*16|0, charReplacement = (char == 'x') ? rand : (rand&0x3|0x8);
+
+      return charReplacement.toString(16);
+    });
+  }
+
+  function saveMatch(guid, matchDetails, callback){
     redisClient.hmset(guid, matchDetails, callback);
   };
 
-  var scheduleMatch = function(matchDetails, guid){
+  function scheduleMatch(matchDetails, guid){
     var matchTypes = {
       singles: { maxNumOfPlayers: 2 },
       doubles: { maxNumOfPlayers: 4 }
@@ -76,15 +111,19 @@ exports.init = function init(app, options) {
       redisClient.get(pendingMatchKey, function(err, pendingGuid){
         redisClient.hmget(pendingGuid, 'names', function(err, pendingOpponentNames){
           if(pendingGuid) {
-            var selectedTableName = pickMatchTable();
+            var matchGuid = createGuid(),
+                selectedTableName = pickMatchTable();
+
             redisClient.multi()
             .del(pendingMatchKey)
-            .hset(pendingGuid, 'opponentNames', matchDetails.names)
-            .hset(guid, 'opponentNames', pendingOpponentNames)
-            .hset(pendingGuid, 'tableName', selectedTableName)
-            .hset(guid, 'tableName', selectedTableName)
-            .expire(pendingGuid, 60 * 60)
-            .expire(guid, 60 * 60)
+            .hset(pendingGuid, 'scheduledMatchGuid', matchGuid)
+            .hset(guid, 'scheduledMatchGuid', matchGuid)
+            .hset(pendingGuid, 'confirmed', 0)
+            .hset(guid, 'confirmed', 0)
+            .hmset(matchGuid, {team1: pendingGuid, team2: guid, tableName: selectedTableName})
+            .expire(matchGuid, 80)
+            .expire(pendingGuid, 80)
+            .expire(guid, 80)
             .exec(repeatFunction);
           } else {
             redisClient.multi()
